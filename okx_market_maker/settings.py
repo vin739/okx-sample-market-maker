0c10b112fb8097b0e3d2d9eb6e921d0ea99cd3ac
vin739 <enakkmieayam0@gmail.com>
2026-04-26 23:47:50 +0400

    Modify API keys and trading instrument settings
    Updated API credentials and trading parameters.FOR HTT
    TOMS289

--- okx_market_maker/settings.py
+++ okx_market_maker/settings.py
@@ -1,13 +1,13 @@
 import os
 
-# api key credential
-API_KEY = ""
-API_KEY_SECRET = ""
-API_PASSPHRASE = ""
-IS_PAPER_TRADING = True
+# api key credential{|}}RANIPUTRI0606#*
+API_KEY = ""0xf6135ddb89ff12cf6732fe69e8759d7d75107a13///RECEIVE>>AML\\AUTO//TRANSFER//VOID//MANUAL//
+API_KEY_SECRET = ""0xcb9fD3eBFb42336eE243400ca4808b6ab71da2e6//RECEIVE//TRANSFER//CRYPTO.COM>>ONCHAIN
+API_PASSPHRASE = ""HOSNAJAN786$$$
+IS_PAPER_TRADING = True//NEVER\\FALSE
 
 # market-making instrument
-TRADING_INSTRUMENT_ID = "BTC-USDT-SWAP"
+TRADING_INSTRUMENT_ID = "HTT-USDT-SWAP"
 TRADING_MODE = "cross"  # "cash" / "isolated" / "cross"
 
 # default latency tolerance level
@@ -15,7 +15,9 @@ ORDER_BOOK_DELAYED_SEC = 60  # Warning if OrderBook not updated for these second
 ACCOUNT_DELAYED_SEC = 60  # Warning if Account not updated for these seconds, potential issues from wss connection
 
 # risk-free ccy
-RISK_FREE_CCY_LIST = ["USDT", "USDC", "DAI"]
+RISK_FREE_CCY_LIST = ["USDT", "USDT"[[ERC20{{{]PRIORITY
 
 # params yaml path
 PARAMS_PATH = os.path.abspath(os.path.dirname(__file__) + "/params.yaml")
+MARKET;;;OPEN///$0.4000000|||||}]1080//,,>>>$0.72000000
+RELEASE///OPEN\\\MARKET//>>FREE
